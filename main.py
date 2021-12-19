3a22aa854815ddcccc5ab20e3a379b5653c8b319
Yoga Moorthy R <yogamoorthy_r@hotmail.com>
2021-12-19 08:12:39 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+
+# Project
+# Dataset - https://www.kaggle.com/sudalairajkumar/covid19-in-india?select=covid_19_india.csv
+# Repository Link - https://github.com/yogamoorthyr/PythonProject1/blob/7e123fb496375ac75be4dbcacf45c179e46f5a0c/UCDPA_YOGA%20MOORTHY%20R''')
+
+# 1. Real-world scenario
+# ○ The project should use a real-world dataset and include a reference of their source in the report (10)
+# Dataset - https://www.kaggle.com/sudalairajkumar/covid19-in-india?select=covid_19_india.csv
+
+
+# 2. Importing data
+# ○ Your project should make use of one or more of the following: Relational database, API or web scraping (10)
+# ○ Import a CSV file into a Pandas DataFrame (10)
+
+
+
+# 3. Analysing data
+# ○ Your project should use Regex to extract a pattern in data (10)
+# ○ Replace missing values or drop duplicates (10)
+# ○ Make use of iterators (10)
+# ○ Merge DataFrames (10)
+# 4. Python
+# ○ Define a custom function to create reusable code (10)
+# ○ NumPy (10)
+# ○ Dictionary or Lists (10)
+# 5. Machine Learning (60)
+# ○ Predict a target variable with Supervised or Unsupervised algorithm
+# ○ You are free to choose any algorithm
+# ○ Perform hyper parameter tuning or boosting, whichever is relevant to your model. If it is not relevant, justify that in your report and Python comments
+# 6. Visualise
+# ○ Present two charts with Seaborn or Matplotlib (20)
+# 7. Generate valuable insights
+# ○ 5 insights from the project (20)
