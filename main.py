f259ed4f2843fc2de2c46d40fc346482d8d2c2f3
Yoga Moorthy R <yogamoorthy_r@hotmail.com>
2021-12-19 08:40:35 +0530

    New Changes 19122021

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
 
 # Project
 # Dataset - https://www.kaggle.com/sudalairajkumar/covid19-in-india?select=covid_19_india.csv
-# Repository Link - https://github.com/yogamoorthyr/PythonProject1/blob/7e123fb496375ac75be4dbcacf45c179e46f5a0c/UCDPA_YOGA%20MOORTHY%20R''')
+# Repository Link - https://github.com/yogamoorthyr/UCDPA_YOGAMOORTHYR/blob/master/main.py
 
 # 1. Real-world scenario
 # ○ The project should use a real-world dataset and include a reference of their source in the report (10)
@@ -21,15 +25,28 @@ import numpy as np
 # ○ Replace missing values or drop duplicates (10)
 # ○ Make use of iterators (10)
 # ○ Merge DataFrames (10)
+
+
+
 # 4. Python
 # ○ Define a custom function to create reusable code (10)
 # ○ NumPy (10)
 # ○ Dictionary or Lists (10)
+
+
+
 # 5. Machine Learning (60)
 # ○ Predict a target variable with Supervised or Unsupervised algorithm
 # ○ You are free to choose any algorithm
 # ○ Perform hyper parameter tuning or boosting, whichever is relevant to your model. If it is not relevant, justify that in your report and Python comments
+
+
+
+
 # 6. Visualise
 # ○ Present two charts with Seaborn or Matplotlib (20)
+
+
+
 # 7. Generate valuable insights
 # ○ 5 insights from the project (20)
